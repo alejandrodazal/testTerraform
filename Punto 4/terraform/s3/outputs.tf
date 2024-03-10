4ff2d37c43a847bdfef8bfe9c869aa44a1a437e0
Alejandro Esteban Daza <alejandrodazal@gmail.com>
2024-03-10 11:55:48 -0500

    Delete Punto 4 directory

--- Punto 4/terraform/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_arn" {
-  description = "ARN del bucket de S3"
-  value       = aws_s3_bucket.punto4_DK.arn
-}
-
-output "bucket_nombre" {
-  description = "Nombre del bucket de S3"
-  value       = aws_s3_bucket.punto4_DK.bucket
-}
