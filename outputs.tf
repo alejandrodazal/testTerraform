7401d493ce525d02b963f93355f4ae917fa5f6cb
Alejandro Esteban Daza <alejandrodazal@gmail.com>
2024-03-10 11:56:14 -0500

    Proyecto

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_punto4" {
+  value       = module.s3_punto4_DK
+  description = "El nombre del bucket ;)."
+}
+
+output "s3_bucket_arn" {
+  value       = module.s3_punto4_DK
+  description = "El ARN del bucket de S3 creado."
+}
