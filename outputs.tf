bc8d167f803186951ce374f88c290bb6fd540236
Alejandro Esteban Daza <alejandrodazal@gmail.com>
2024-03-11 13:13:29 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_punto4" {
-  value       = module.s3_punto4_DK
-  description = "El nombre del bucket ;)."
-}
-
-output "s3_bucket_arn" {
-  value       = module.s3_punto4_DK
-  description = "El ARN del bucket de S3 creado."
-}
